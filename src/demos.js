bbc8f295a79187650af34daeadee8cab117abe49
Brandon Semilla <semibran@gmail.com>
2018-11-16 14:02:23 -0800

    simplify ease in/out functions

--- src/demos.js
+++ src/demos.js
@@ -1,8 +1,4 @@
-import * as quadratic from "../lib/quadratic"
-import * as cubic from "../lib/cubic"
-import * as sinusoidal from "../lib/sinusoidal"
-import * as exponential from "../lib/exponential"
-import * as circular from "../lib/circular"
+import * as easing from "../lib/easing"
 
 export default [
 	{
@@ -11,86 +7,86 @@ export default [
 		formula: "t"
 	},
 	{
-		fn: quadratic.easeIn,
+		fn: easing.quadratic.easeIn,
 		title: "quadratic ease in",
 		formula: "t * t"
 	},
 	{
-		fn: quadratic.easeOut,
+		fn: easing.quadratic.easeOut,
 		title: "quadratic ease out",
 		formula: "-t * (t - 2)"
 	},
 	{
-		fn: quadratic.easeInOut,
+		fn: easing.quadratic.easeInOut,
 		title: "quadratic ease in/out",
 		formula: `t < 0.5
-  ? t * t * 2
-  : -((t = t * 2 - 1) * (t - 2) - 1) / 2`
+  ? easeIn(t * 2) / 2
+  : (easeOut(t * 2 - 1) + 1) / 2`
 	},
 	{
-		fn: cubic.easeIn,
+		fn: easing.cubic.easeIn,
 		title: "cubic ease in",
-		formula: "t^3"
+		formula: "t * t * t"
 	},
 	{
-		fn: cubic.easeOut,
+		fn: easing.cubic.easeOut,
 		title: "cubic ease out",
-		formula: "(t - 1)^3 + 1"
+		formula: "pow(t - 1, 3) + 1"
 	},
 	{
-		fn: cubic.easeInOut,
+		fn: easing.cubic.easeInOut,
 		title: "cubic ease in/out",
 		formula: `t < 0.5
-  ? t^3 * 4
-  : ((t * 2 - 2)^3 + 2) / 2`
+  ? easeIn(t * 2) / 2
+  : (easeOut(t * 2 - 1) + 1) / 2`
 	},
 	{
-		fn: sinusoidal.easeIn,
+		fn: easing.sinusoidal.easeIn,
 		title: "sinusoidal ease in",
 		formula: "cos(π + π / 2 * t) + 1"
 	},
 	{
-		fn: sinusoidal.easeOut,
+		fn: easing.sinusoidal.easeOut,
 		title: "sinusoidal ease out",
 		formula: "sin(π / 2 * t)"
 	},
 	{
-		fn: sinusoidal.easeInOut,
+		fn: easing.sinusoidal.easeInOut,
 		title: "sinusoidal ease in/out",
 		formula: "(cos(π + π * t) + 1) / 2"
 	},
 	{
-		fn: exponential.easeIn,
+		fn: easing.exponential.easeIn,
 		title: "exponential ease in",
-		formula: "2 ^ (10 * (t - 1))"
+		formula: "pow(2, 10 * (t - 1))"
 	},
 	{
-		fn: exponential.easeOut,
+		fn: easing.exponential.easeOut,
 		title: "exponential ease out",
-		formula: "-(2 ^ (-10 * t)) + 1"
+		formula: "-pow(2, -10 * t) + 1"
 	},
 	{
-		fn: exponential.easeInOut,
+		fn: easing.exponential.easeInOut,
 		title: "exponential ease in/out",
-		formula: `(t = t * 2 - 1) < 0
-  ? 2 ^ (10 * t) / 2
-  : (-(2 ^ (-10 * t) + 2) / 2`
+		formula: `t < 0.5
+  ? easeIn(t * 2) / 2
+  : (easeOut(t * 2 - 1) + 1) / 2`
 	},
 	{
-		fn: circular.easeIn,
+		fn: easing.circular.easeIn,
 		title: "circular ease in",
-		formula: "-sqrt(1 - t^2)"
+		formula: "-sqrt(1 - pow(t, 2))"
 	},
 	{
-		fn: circular.easeOut,
+		fn: easing.circular.easeOut,
 		title: "circular ease out",
-		formula: "sqrt(1 - (t - 1)^2)"
+		formula: "sqrt(1 - pow(t - 1, 2))"
 	},
 	{
-		fn: circular.easeInOut,
+		fn: easing.circular.easeInOut,
 		title: "circular ease in/out",
 		formula: `t < 0.5
-  ? -(sqrt(1 - (t * 2)^2) - 1)/ 2
-  : (sqrt(1 - (t * 2 - 2)^2) + 1) / 2`
+  ? easeIn(t * 2) / 2
+  : (easeOut(t * 2 - 1) + 1) / 2`
 	}
 ]
